732509d5e49ea7a2fbf4db4b7f3094e474459754
MarcAngelo Perez <mpere343@stu.southtexascollege.edu>
2025-11-09 17:43:12 -0600

    delete is working but commented

--- controller/employeeController.js
+++ controller/employeeController.js
@@ -20,7 +20,7 @@ export const create = async (req, res) => {
 
 export const fetch = async (req, res) => {
   try {
-    
+
     const employees = await Employee.find();
     if (employees.length === 0) {
       return res.status(404).json({ message: "Employees not found!" });
@@ -31,3 +31,37 @@ export const fetch = async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
+
+export const update = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const employeeExist = await Employee.findOne({ _id: id });
+
+    if (!employeeExist) {
+      return res.status(404).json({ message: "Employee not found" });
+    }
+
+    const updateEmployee = await Employee.findByIdAndUpdate(id, req.body, { new: true });
+    res.status(201).json(updateEmployee);
+
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+// export const deleteEmployee = async (req, res) => {
+//   try {
+//     const id = req.params.id;
+//     const employeeExist = await Employee.findOne({ _id: id });
+
+//     if (!employeeExist) {
+//       return res.status(404).json({ message: "Employee not found" });
+//     }
+
+//     await Employee.findByIdAndDelete(id);
+//     res.status(201).json({ message: "Employee terminated!!" });
+
+//   } catch (error) {
+//     res.status(500).json({ error: "Internal Server Error" });
+//   }
+// };
